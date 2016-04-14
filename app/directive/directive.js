1509cf7094ee8f107f05b5b111389c6ee17a308b
Desh Deepak <deshdeepak2000@gmail.com>
2016-04-14 18:48:22 +0530

    angular directive example

---
+++ app/directive/directive.js
@@ -0,0 +1,24 @@
+var myDir=angular.module('appDirective',[]);
+
+myDir.directive("alphabetOnly",function(){
+	return{
+			require:'ngModel',
+			restrict:'A',
+			link: function(scope, element, attrs, modelCtrl) {
+			modelCtrl.$parsers.push(function (inputValue) {
+            // this next if is necessary for when using ng-required on your input. 
+            // In such cases, when a letter is typed first, this parser will be called
+            // again, and the 2nd time, the value will be undefined
+            if (inputValue == undefined) return '' 
+            var transformedInput = inputValue.replace(/[^A-Za-z]/g, ''); 
+            if (transformedInput!=inputValue) {
+              modelCtrl.$setViewValue(transformedInput);
+              modelCtrl.$render();
+            }         
+
+            return transformedInput;         
+				});
+			}
+         }
+		
+});
